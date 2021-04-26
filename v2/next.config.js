e34032f84c339ddf1e62c667e898eae7d5698d93
samsisle <samko9522@gmail.com>
2021-04-26 16:44:04 -0700

    Update next.config.js

--- v2/next.config.js
+++ v2/next.config.js
@@ -4,15 +4,5 @@ module.exports = {
   basePath: basePath,
   future: {
     webpack5: true
-  },
-  // async rewrites() {
-  //   return {
-  //     fallback: [
-  //       {
-  //         source: '/:path*',
-  //         destination: 'https://vercel-routing-example-v1-cra.vercel-support.app',
-  //       },
-  //     ]
-  //   }
-  // }
+  }
 }
